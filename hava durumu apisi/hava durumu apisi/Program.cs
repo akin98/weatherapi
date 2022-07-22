fab3aff9241d9f4ad77fb1cf1d8a369d5a3f1abe
akın sultar <109798864+akin98@users.noreply.github.com>
2022-07-22 14:23:44 +0300

    weatherapi
    hava durumu apisini çektiğimiz proje

---
+++ hava durumu apisi/hava durumu apisi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq; //xml kütüphanesini  tanımladık
+namespace hava_durumu_apisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string api = "c08a224d877e38c4825dbfee9c489cd6";//hava durumu sitesinden aldığımız key bu site key olarak veriyo normalda referans
+            string baglan = "https://api.openweathermap.org/data/2.5/weather?q=%C4%B0zmir&mode=xml&lang=tr&units=metric&appid=" + api; // hocanın attığı bağlantı linki
+            XDocument hava=XDocument.Load(baglan); //xml olduğu için bunu ekledik
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;//var bilinmeyen değişkenler için kullanılır
+            Console.WriteLine("izmir hava durumu" + sicaklik);
+            Console.Read();
+        }
+    }
+}
